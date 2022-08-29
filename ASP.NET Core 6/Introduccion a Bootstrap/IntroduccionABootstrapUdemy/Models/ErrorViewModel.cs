85a39b6e787487dc0f2f625779166e9f1663e487
Felipe Gavilan <elmejor_887@hotmail.com>
2022-08-29 16:15:53 -0400

    Agregado nuevo modulo

---
+++ ASP.NET Core 6/Introduccion a Bootstrap/IntroduccionABootstrapUdemy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntroduccionABootstrapUdemy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
